27c7c46a6d1acbf91ec2cebca72a02c51d2c6c56
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-04-08 18:44:10 -0500

    Creando los esquemas para productos y categorias

---
+++ src/data/mongo/models/category.model.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const CategorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    available: {
+        type: Boolean,
+        default: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "User is required"],
+    },
+
+
+});
+
+export const CategoryModel = mongoose.model("Category", CategorySchema);
