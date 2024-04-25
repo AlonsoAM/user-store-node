d1ed2373e22ca1e714578cfbd6f7ac2141bd0c0c
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-04-25 08:00:22 -0500

    Poblar la base de datos con un seed

--- src/data/seed/seed.ts
+++ src/data/seed/seed.ts
@@ -1,5 +1,9 @@
 import {MongoDB} from "../mongo/mongo-db";
 import {envs} from "../../config";
+import {UserModel} from "../mongo/models/user.model";
+import {CategoryModel} from "../mongo/models/category.model";
+import {ProductModel} from "../mongo/models/product.model";
+import {seedData} from "./data";
 
 (async () => {
     await MongoDB.connect({
@@ -11,12 +15,33 @@ import {envs} from "../../config";
     await MongoDB.disconnect();
 })();
 
-async function main(){
+const randomBetween0andX = (x: number) => Math.floor(Math.random() * x);
+
+async function main() {
+
+    // 0. Borrar todo
+    await Promise.all([
+        UserModel.deleteMany({}),
+        CategoryModel.deleteMany({}),
+        ProductModel.deleteMany({})
+    ])
+
     // 1. Crear usuarios
+    const users = await UserModel.insertMany(seedData.users);
 
     // 2. Crear categorias
+    const categories = await CategoryModel.insertMany(seedData.categories.map(category => ({
+        ...category,
+        user: users[randomBetween0andX(users.length)]._id
+    })));
 
     // 3. Crear productos
+    const products = await ProductModel.insertMany(seedData.products.map(product => ({
+        ...product,
+        category: categories[randomBetween0andX(categories.length)]._id,
+        user: users[randomBetween0andX(users.length)]._id
+    })));
+
 
     console.log('Seed finalizado');
 }
